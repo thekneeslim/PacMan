666e3ca058e891786e1513829156c0d2a3775dc0
Denise Lim <hiko_seijuro1000@Denises-MacBook-Air.local>
2016-08-29 17:16:57 +0800

    direction works

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 var y = 200;
 var x = 200;
 
+var MOVING_UP = false;
+var MOVING_DOWN = false;
+var MOVING_LEFT = false;
+var MOVING_RIGHT = false;
+
 //  GAME START CONTROL
 window.addEventListener('load', function (event) {
   initCanvas();
@@ -9,38 +14,20 @@ window.addEventListener('load', function (event) {
 // LISTENING FOR KEYBOARD INPUTS
 document.addEventListener('keydown', function (event) {
   var keyPress = event.keyCode;
-  if (keyPress === 38 || keyPress === 40) {
-    xAxis(keyPress);
-  } else if (keyPress === 37 || keyPress === 39) {
-    yAxis(keyPress);
+  MOVING_UP = false;
+  MOVING_DOWN = false;
+  MOVING_LEFT = false;
+  MOVING_RIGHT = false;
+  if (keyPress === 38) {
+    MOVING_UP = true;
+  } else if (keyPress === 40) {
+    MOVING_DOWN = true;
+  } else if (keyPress === 37) {
+    MOVING_LEFT = true;
+  } else if (keyPress === 39) {
+    MOVING_RIGHT = true;
   }
 });
-//   if (keyPress === 38) {
-//     y--;
-//   } else if (keyPress === 40) {
-//     y++;
-//   } else if (keyPress === 37) {
-//     x--;
-//   } else if (keyPress === 39) {
-//     x++;
-//   }
-// });
-
-function xAxis (m) {
-  if(m === 37) {
-    x--;
-  } else if (m === 39) {
-    x++;
-  }
-}
-
-function yAxis (n) {
-  if(n === 38) {
-    return x--;
-  } else if (n === 40) {
-    return x++;
-  }
-}
 
 // INITIALIZING CANVAS PROPERTIES
 function initCanvas () {
@@ -51,11 +38,26 @@ function initCanvas () {
   var cH = ctx.canvas.height;
 
   function movePM () {
+    if (MOVING_DOWN) {
+        y++;
+    }
+
+    if (MOVING_UP) {
+        y--;
+    }
+
+    if (MOVING_LEFT) {
+        x--;
+    }
+
+    if (MOVING_RIGHT) {
+        x++;
+    }
+
     ctx.save();
     ctx.clearRect(0, 0, cW, cH);
     // DRAWING HERE
     ctx.drawImage(pacman, x, y, 30, 30);
-    // x++;
     // RESTORING
     ctx.restore();
   }
