abd2811aa6b3013d1bc2b7ca69f7a1d3da949202
Denise Lim <hiko_seijuro1000@Denises-MacBook-Air.local>
2016-08-30 10:54:20 +0800

    borders are functioning

--- script.js
+++ script.js
@@ -10,14 +10,23 @@ var MOVING_RIGHT = false;
 window.addEventListener('load', function (event) {
   initCanvas();
 });
-
-// LISTENING FOR KEYBOARD INPUTS
-document.addEventListener('keydown', function (event) {
-  var keyPress = event.keyCode;
+// TO RESTRICT MOVEMENT OF PACMAN
+function defaultMovements() {
   MOVING_UP = false;
   MOVING_DOWN = false;
   MOVING_LEFT = false;
   MOVING_RIGHT = false;
+}
+
+function borderCollide() {
+
+}
+
+// LISTENING FOR KEYBOARD INPUTS
+document.addEventListener('keydown', function (event) {
+  var keyPress = event.keyCode;
+  defaultMovements();
+
   if (keyPress === 38) {
     MOVING_UP = true;
   } else if (keyPress === 40) {
@@ -31,38 +40,82 @@ document.addEventListener('keydown', function (event) {
 
 // INITIALIZING CANVAS PROPERTIES
 function initCanvas () {
-  var pacman = new Image();
-  pacman.src = 'pacman.png';
+  // var pacman = new Image();
+  // pacman.src = 'pacman.png';
   var ctx = document.getElementById('gCanvas').getContext('2d');
   var cW = ctx.canvas.width;
   var cH = ctx.canvas.height;
 
   function movePM () {
     if (MOVING_DOWN) {
+      if (y === cH - 20) {
+        MOVING_DOWN = false;
+        y = y;
+      } else {
         y++;
+      }
     }
-
     if (MOVING_UP) {
+      if (y === 20) {
+        MOVING_UP = false;
+        y = y;
+      } else {
         y--;
+      }
     }
-
     if (MOVING_LEFT) {
+      if (x === 20) {
+        MOVING_LEFT = false;
+        x = x;
+      } else {
         x--;
+      }
     }
-
     if (MOVING_RIGHT) {
+      if (x === cW - 20) {
+        MOVING_RIGHT = false;
+        x = x;
+      } else {
         x++;
+      }
     }
 
     ctx.save();
     ctx.clearRect(0, 0, cW, cH);
+
     // DRAWING HERE
-    ctx.drawImage(pacman, x, y, 30, 30);
+    ctx.beginPath();
+    ctx.arc(x, y, 15, 0.25*Math.PI, 1.7*Math.PI);
+    ctx.lineTo(x-8,y);
+    ctx.stroke();
+    ctx.closePath();
+    ctx.fill();
+
+    // ctx.drawImage(pacman, x, y, 30, 30);
     // RESTORING
     ctx.restore();
   }
-  var movePMInterval = setInterval(movePM, 15);
+  var movePMInterval = setInterval(movePM, 10);
 }
+
+
+
+// DRAWING PACMAN
+// function drawPacMan () {
+//   ctx.beginPath();
+//   ctx.arc(100, 100, 50, 0.25 * Math.PI, 1.25 * Math.PI, false);
+//   ctx.fillStyle = "rgb(255, 255, 0)";
+//   ctx.fill();
+//
+//   ctx.beginPath();
+//   ctx.arc(100, 100, 50, 0.75 * Math.PI, 1.75 * Math.PI, false);
+//   ctx.fill();
+//
+//   ctx.beginPath();
+//   ctx.arc(100, 75, 10, 0, 2 * Math.PI, false);
+//   ctx.fillStyle = "rgb(0, 0, 0)";
+//   ctx.fill();
+// }
 
 function hitWall() {
   x = x;
