d594be9e4058a8c59a834d9d6df44059eadbf045
Denise Lim <hiko_seijuro1000@Denises-MacBook-Air.local>
2016-08-30 16:37:43 +0800

    maze grid is up

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var y = 220;
+var y = 420;
 var x = 220;
 
 var pMAN    = [x, y];
@@ -22,12 +22,37 @@ window.addEventListener('load', function (event) {
   initCanvas();
 });
 
-// TO RESTRICT MOVEMENT OF PACMAN
-function defaultMovements () {
-  MOVING_UP = false;
-  MOVING_DOWN = false;
-  MOVING_LEFT = false;
-  MOVING_RIGHT = false;
+// INITIALIZING CANVAS PROPERTIES
+function initCanvas () {
+  // var pacman = new Image();
+  // pacman.src = 'pacman.png';
+  maze();
+  function pM () {
+    drawPM();
+    moveIcons();
+    pMAN[0] = x;
+    pMAN[1] = y;
+  }
+  var movePMInterval = setInterval(pM, 10);
+}
+
+function drawPM() {
+  ctx.save();
+  // ctx.clearRect(0, 0, cW, cH);
+
+  // DRAWING HERE
+  ctx.beginPath();
+  ctx.fillStyle = "yellow";
+  // ctx.shadowcolour = 'rgba(0,0,0,0)';
+  ctx.arc(x, y, 15, 0.25*Math.PI, 1.7*Math.PI);
+  ctx.lineTo(x-8,y);
+  ctx.stroke();
+  ctx.closePath();
+  ctx.fill();
+
+  // ctx.drawImage(pacman, x, y, 30, 30);
+  // RESTORING
+  ctx.restore();
 }
 
 // FORMING GRIDS & NAMING THEM IN ARRAY
@@ -35,12 +60,96 @@ for(var i = 0; i < cH; i += 40) {
   var arrayK = [];
   for(var k = 0; k < cW; k += 40) {
 
-    ctx.fillStyle = 'orange';
     ctx.strokeRect(k, i, 40, 40);
-    ctx.fillRect(k, i, 40, 40);
     arrayK.push(0);
   }
   cGRID.push(arrayK);
+}
+
+// DRAWING THE MAZE
+function maze() {
+  ctx.fillStyle = "orange";
+  ctx.fillRect(40, 40, 120, 80);
+  ctx.fillRect(200, 0, 40, 120);
+  ctx.fillRect(280, 40, 120, 80);
+  ctx.fillRect(40, 160, 80, 80);
+  ctx.fillRect(320, 160, 80, 80);
+  ctx.fillRect(0, 280, 80, 40);
+  ctx.fillRect(360, 280, 80, 40);
+  ctx.fillRect(0, 360, 80, 80);
+  ctx.fillRect(360, 360, 80, 80);
+  ctx.fillRect(40, 480, 40, 40);
+  ctx.fillRect(360, 480, 40, 40);
+  ctx.fillRect(120, 440, 80, 40);
+  ctx.fillRect(240, 440, 80, 40);
+  // GHOST HOME
+  ctx.beginPath();
+  ctx.moveTo(200, 280);
+  ctx.lineTo(120, 280);
+  ctx.lineTo(120, 400);
+  ctx.lineTo(320, 400);
+  ctx.lineTo(320, 280);
+  ctx.lineTo(240, 280);
+  ctx.lineTo(240, 320);
+  ctx.lineTo(280, 320);
+  ctx.lineTo(280, 360);
+  ctx.lineTo(160, 360);
+  ctx.lineTo(160, 320);
+  ctx.lineTo(200, 320);
+  ctx.fill();
+  ctx.closePath();
+  // T ABOVE GHOST HOME
+  ctx.beginPath();
+  ctx.moveTo(160, 160);
+  ctx.lineTo(160, 200);
+  ctx.lineTo(200, 200);
+  ctx.lineTo(200, 240);
+  ctx.lineTo(240, 240);
+  ctx.lineTo(240, 200);
+  ctx.lineTo(280, 200);
+  ctx.lineTo(280, 160);
+  ctx.fill();
+  ctx.closePath();
+  // BOTTOM LEFT L
+  ctx.beginPath();
+  ctx.moveTo(40, 560);
+  ctx.lineTo(40, 640);
+  ctx.lineTo(120, 640);
+  ctx.lineTo(120, 600);
+  ctx.lineTo(80, 600);
+  ctx.lineTo(80, 560);
+  ctx.fill();
+  ctx.closePath();
+// BOTTOM LEFT INVERTED L
+  ctx.beginPath();
+  ctx.moveTo(120, 520);
+  ctx.lineTo(120, 560);
+  ctx.lineTo(160, 560);
+  ctx.lineTo(160, 640);
+  ctx.lineTo(200, 640);
+  ctx.lineTo(200, 520);
+  ctx.fill();
+  ctx.closePath();
+  // BOTTOM right INVERTED L
+  ctx.beginPath();
+  ctx.moveTo(240, 520);
+  ctx.lineTo(240, 640);
+  ctx.lineTo(280, 640);
+  ctx.lineTo(280, 560);
+  ctx.lineTo(320, 560);
+  ctx.lineTo(320, 520);
+  ctx.fill();
+  ctx.closePath();
+  // BOTTOM RIGHT L
+  ctx.beginPath();
+  ctx.moveTo(360, 560);
+  ctx.lineTo(360, 600);
+  ctx.lineTo(320, 600);
+  ctx.lineTo(320, 640);
+  ctx.lineTo(400, 640);
+  ctx.lineTo(400, 560);
+  ctx.fill();
+  ctx.closePath();
 }
 
 // LISTENING FOR KEYBOARD INPUTS
@@ -57,19 +166,6 @@ document.addEventListener('keydown', function (event) {
     MOVING_RIGHT = true;
   }
 });
-
-// INITIALIZING CANVAS PROPERTIES
-function initCanvas () {
-  // var pacman = new Image();
-  // pacman.src = 'pacman.png';
-  function pM () {
-    drawPM();
-    moveIcons();
-    pMAN[0] = x;
-    pMAN[1] = y;
-  }
-  var movePMInterval = setInterval(pM, 10);
-}
 
 // PM CAN ONLY MOVE IN GRIDS OF 20 OR 40
 function moveIcons () {
@@ -111,19 +207,10 @@ function moveIcons () {
   }
 }
 
-function drawPM() {
-  ctx.save();
-  // ctx.clearRect(0, 0, cW, cH);
-
-  // DRAWING HERE
-  ctx.beginPath();
-  ctx.arc(x, y, 15, 0.25*Math.PI, 1.7*Math.PI);
-  ctx.lineTo(x-8,y);
-  ctx.stroke();
-  ctx.closePath();
-  ctx.fill();
-
-  // ctx.drawImage(pacman, x, y, 30, 30);
-  // RESTORING
-  ctx.restore();
+// TO RESTRICT MOVEMENT OF PACMAN
+function defaultMovements () {
+  MOVING_UP = false;
+  MOVING_DOWN = false;
+  MOVING_LEFT = false;
+  MOVING_RIGHT = false;
 }
